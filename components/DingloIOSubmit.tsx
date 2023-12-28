f02a4645e3b1177e85318b798adb74d16451d16f
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-12-28 13:04:12 +0200

    feat - emit typing event

--- components/DingloIOSubmit.tsx
+++ components/DingloIOSubmit.tsx
@@ -7,7 +7,7 @@ import {useForm} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DingloIOMessageValidator } from "@/validators";
 import dingloIO from "@/dinglo-io";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Separator } from "@radix-ui/react-separator";
 import { dingloMessage } from "@/types";
 
@@ -22,18 +22,34 @@ export const DingloIOSubmit = ({setMessages}: DingloIOSubmit) => {
     });
 
     useEffect(()=>{
-        console.log(errors);
-        
-    },[errors]);
+
+    },[]);
 
   return (
     <div>
         <Separator className={`h-[1.5px] ${Object.keys(errors).length>0 ? "bg-red-500":"bg-softBlue"}`}/>
         <form onSubmit={handleSubmit((data)=>{
             setMessages(prev=>[...prev, {isAgent: false, message:data.message, messagedAt:new Date(Date.now()).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }), isNew:false}])
-            dingloIO.respond(data.message)
+            dingloIO.respond(data.message);
+            if(!dingloIO.socket) return;
+            dingloIO.socket.emit("typing",{})
             })} className="pt-3 flex items-center justify-between pb-2">
-            <Input {...register("message")} placeholder={errors.message?.message ? errors.message.message as string:"Enter your message..."} className="border-none" />
+            <Input {...register("message")} onChange={(e)=>{
+                if(e.target.value && e.target.value!==""){
+
+                    setTimeout(()=>{
+                        if(!dingloIO.socket) return;
+
+                        dingloIO.socket.emit("typing",{isTyping: true});
+                    },500);
+                }else{
+                    setTimeout(()=>{
+                        if(!dingloIO.socket) return;
+
+                        dingloIO.socket.emit("typing",{isTyping: false});
+                    },500);
+                }
+            }} placeholder={errors.message?.message ? errors.message.message as string:"Enter your message..."} className="border-none" />
             <Button variant={"outline"} className="group">
             <Send className="w-5 h-5 text-softBlue group-hover:text-white" />
             </Button>
